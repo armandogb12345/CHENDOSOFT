f14d6868078ad15301e3fe19d47ed1a42849294c
calmarazs <cintiaalm.25@gmail.com>
2019-12-04 22:36:01 -0600

    se añadieron cambios componente estatus de revision del informe de titulacion ASCG y se cambio la tabla evaluacion para agregar una llave foranea

---
+++ 04_BaseDeDatos/evaluacion.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 05-12-2019 a las 04:31:40
+-- Versión del servidor: 5.7.23
+-- Versión de PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_tsp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluacion`
+--
+
+DROP TABLE IF EXISTS `evaluacion`;
+CREATE TABLE IF NOT EXISTS `evaluacion` (
+  `idEvaluacion` int(11) NOT NULL AUTO_INCREMENT,
+  `Criterios` varchar(45) NOT NULL,
+  `Puntaje` decimal(3,0) NOT NULL,
+  `Expediente_idExpediente` int(11) NOT NULL,
+  `Documento_idDocumento` int(11) NOT NULL,
+  PRIMARY KEY (`idEvaluacion`),
+  KEY `fk_Evaluacion_Expediente1_idx` (`Expediente_idExpediente`),
+  KEY `Documento_idDocumento` (`Documento_idDocumento`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `evaluacion`
+--
+
+INSERT INTO `evaluacion` (`idEvaluacion`, `Criterios`, `Puntaje`, `Expediente_idExpediente`, `Documento_idDocumento`) VALUES
+(1, 'Hacen falta datos', '0', 1, 1),
+(2, 'Muy bien', '1', 1, 2),
+(3, 'No revisado', '0', 1, 3);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `evaluacion`
+--
+ALTER TABLE `evaluacion`
+  ADD CONSTRAINT `fk_Evaluacion_Expediente1` FOREIGN KEY (`Expediente_idExpediente`) REFERENCES `expediente` (`idExpediente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
