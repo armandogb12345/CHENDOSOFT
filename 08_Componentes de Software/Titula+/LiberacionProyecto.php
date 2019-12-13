9500eb2e44b14e30381257146e7ae51c699be72e
Fabian Balderas <31578422+Foxtrot16@users.noreply.github.com>
2019-12-12 23:42:36 -0600

    Codigo Liberacion de proyecto

---
+++ 08_Componentes de Software/Titula+/LiberacionProyecto.php
@@ -0,0 +1,101 @@
+<?php 
+include_once "SQLConsultas.php";
+//@session_destroy();
+session_start();
+$sql = new SQLConsultas();
+if ($_SESSION['id_sesion'] == null|| $_SESSION['id_sesion'] == '' ) {   
+    header("Location:index.php");
+}
+
+if ($_GET["id"] != null &&
+    $_GET["id"] != '' &&
+    $_GET["type"] != '' && $_GET["type"] != '') {
+    $id_user = $_GET["id"];
+    $type = $_GET["type"];
+    $username = $sql->consultarUsuarioXID($id_user);
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+        <script src="js/jquery-latest.js"></script>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Validacion de requisitos del estudiante</title>
+        <script src="js/jquery.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/all.js"></script>
+        <link rel="stylesheet" href="css/cabecePerfil.css">
+        <script src="js/jquery-latest.js"></script>
+        <script src="js/jquery.js"></script>
+</head>
+<body style="background: url('images/milestones_background.jpg');">
+        <header class= "header">
+                <nav>
+                <a href="#"><label>Liberacion del proyecto de: <?php echo $username; ?> :</label> </a>
+
+                <a href=".php"  class="btn btn-primary">
+                        <i class="fas fa-power-off"></i> Cerrar sesión</a>
+                </nav>
+        </header>
+        <br><br><br>
+        <?php
+
+        echo $type;
+        echo $id_user;       
+        $servidor="localhost";
+		$usuario="root";
+		$clave="root";
+		$bd="bd_tsp";
+	    $conexion = mysqli_connect($servidor,$usuario,$clave,$bd);
+        $consulta="SELECT documento.Titulo, docente.NombreDocente, estudiante.NombreEstudiante, expediente.Nombre, jefedepartamento.NombreDep , documento.Aprobado
+		FROM expediente inner join estudiante inner join evaluacion inner join documento inner join docente_has_expediente
+		WHERE estudiante.Login_idLogin='$id_user' AND expediente.Estudiante_idEstudiante=estudiante.idEstudiante 
+		AND documento.idDocumento=evaluacion.Documento_idDocumento 
+		AND evaluacion.Expediente_idExpediente=expediente.idExpediente 
+        AND docente_has_expediente.Docente_idDocente=evaluacion.Docente_idDocente";
+        $resultado = mysqli_query($conexion,$consulta);
+        ?>
+        <table class="table table-striped table-dark">
+            <thead >
+                <tr>
+                <th scope="col">Titulo del documento</th>
+                <th scope="col">Nombre del asesor/revisor</th>
+                <th scope="col">Nombre del estudiante</th>
+                <th scope="col">Nombre del expediente</th>
+                <th scope="col">Departamento</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                if(mysqli_num_rows($resultado)>0){
+                    while($r = mysqli_fetch_array($resultado)){
+                        echo '<tr>';
+                        echo '<td>'.$r['Titulo'].'</td>';
+                        echo '<td>'.$r['NombreDocente'].'</td>';
+                        echo '<td>'.$r['NombreEstudiante'].'</td>';
+                        echo '<td>'.$r['Nombre'].'</td>';
+                        echo '<td>'.$r['NombreDep'].'</td>';
+                    }
+                }
+            ?>
+            <label><input type="radio" name="validado" value="Validado"required>Validar</label><br>
+            <label><input type="radio" name="validado" value="No validado">No validar</label>
+            </tbody>
+        </table>
+        <?php echo '<a class="btn btn-primary" href="principal.php?id='.$id_user.'&type='.$type.'"> Aceptar</a>';
+        	   if(isset($_POST[$type])){
+        	   		if(isset($_POST['validado'] && $_POST['validado'] == 'Validado')){
+            			$consultaValidacion = "update documento set Validado='Si'";
+            			$resultado = mysqli_query($conexion, $consultaValidacion);
+            		}else {
+            			$consultaValidacion = "update documento set Validado='No'";
+            			$resultado = mysqli_query($conexion, $consultaValidacion);
+            		}
+        	   }
+        ?>
+</body>
+</html>
